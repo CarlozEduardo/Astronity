de4a96f90cc8f93a6126a607ace0ff61b07ff138
CarlozEduardo <carlos.quinteros@sptech.school>
2023-11-11 20:15:30 -0300

    astronity

---
+++ projeto/src/database/script.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE astronity;
+
+USE astronity;
+
+CREATE TABLE usuario (
+idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(50),
+email VARCHAR(50),
+senha VARCHAR(50),
+telefone CHAR(20),
+dtNasc DATE);
+
+CREATE TABLE postagem (
+idPost INT PRIMARY KEY AUTO_INCREMENT,
+titulo VARCHAR (50),
+descricao VARCHAR(200),
+fkUsuario INT,
+CONSTRAINT fkPost FOREIGN KEY (fkUsuario)
+REFERENCES usuario(idUsuario));
+
+CREATE TABLE comentario (
+idComentario INT PRIMARY KEY AUTO_INCREMENT,
+descricao VARCHAR(100),
+fkUsuario INT,
+fkPost INT,
+CONSTRAINT fkPerfil FOREIGN KEY (fkUsuario)
+REFERENCES usuario(idUsuario),
+CONSTRAINT fkComentario FOREIGN KEY (fkPost)
+REFERENCES postagem(idPost));
